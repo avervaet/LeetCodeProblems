c849a3daae8a9d9c9d9d911ce01e3a6540b0e9af
avervaet <arthur.vervaet@cognitive-matchbox.com>
2019-03-11 12:06:38 +0100

    problem 12 and 13

---
+++ 13_Roman_to_Integer.py
@@ -0,0 +1,12 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        result = 0
+        d = {'I':1, 'IV':4 , 'V': 5, 'IX': 9, 'X':10, 'XL':40, 'L':50, 'XC':90, 'C':100, 'CD':400, 'D':500, 'CM':900, 'M':1000}
+        specials = ['CM', 'CD', 'XC', 'XL', 'IX', 'IV']
+        for special in specials:
+            if special in s:
+                result += d[special]
+                s = s.replace(special, '')
+        for e in s:
+            result += d[e]
+        return result
